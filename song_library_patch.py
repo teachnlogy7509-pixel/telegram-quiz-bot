51de8cba38c36de8d74be4b3d4c57eb93ad8014c
teachnlogy7509-pixel <teachnlogy7509@gmail.com>
2026-09-16 22:55:24 +0530

    Make song Drive URL patch robust

--- song_library_patch.py
+++ song_library_patch.py
@@ -4,16 +4,11 @@ from pathlib import Path
 song = Path('song_library_worker.py')
 if song.exists():
     source = song.read_text()
-    source = re.sub(
-        r'f"\\{\\{https://www\\.googleapis\\.com/drive/v3/files/\\{file_id\\}\\}\\}\\}/permissions\\?fields=id"',
-        'f"https://www.googleapis.com/drive/v3/files/{file_id}/permissions?fields=id"',
-        source,
-    )
-    source = re.sub(
-        r'f"\\{\\{https://drive\\.google\\.com/file/d/\\{file_id\\}\\}\\}\\}/view\\?usp=sharing"',
-        'f"https://drive.google.com/file/d/{file_id}/view?usp=sharing"',
-        source,
-    )
+    file_id_expr = chr(123) + 'file_id' + chr(125)
+    good_permission = 'f"https://www.googleapis.com/drive/v3/files/' + file_id_expr + '/permissions?fields=id"'
+    good_view = 'f"https://drive.google.com/file/d/' + file_id_expr + '/view?usp=sharing"'
+    source = re.sub(r'f"\{+https://www\.googleapis\.com/drive/v3/files/\{file_id\}+}/permissions\?fields=id"', good_permission, source)
+    source = re.sub(r'f"\{+https://drive\.google\.com/file/d/\{file_id\}+/view\?usp=sharing"', good_view, source)
     song.write_text(source)
 
 worker = Path('question_archive_worker.py')
